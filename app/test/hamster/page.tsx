ec7e9c1a4b3771a292c4d7b0c926ab48866f9624
michaelnirgadguy <michael.nirgadguy@gmail.com>
2025-12-17 23:24:33 +0200

    Delete app/test/hamster/page.tsx

--- app/test/hamster/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HamsterTest() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="hamster-wheel hamster-wheel--small" />
-    </div>
-  );
-}
